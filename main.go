9c723f5667c5d18f30b55e57e5fbb5bd92633cf7
Dipansu <dipansu95@gmail.com>
2024-09-26 15:31:50 -0400

    added var

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	checkstr := "Welcome to Group A's Week 4 Project!"
 	fmt.Println("Welcome to Group A's Week 4 Project!")
-	fmt.Println(Reversedstr("Welcome to Group A's Week 4 Project!"))
+	fmt.Println(Reversedstr(checkstr))
+
 }
