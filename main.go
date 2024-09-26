bc2d7617359d870b9d7ad4cf9cf73501d4652ad1
Swaroopkrishna Menon <menonswaroop9517@gmail.com>
2024-09-26 16:01:44 -0400

    Editing Main File with proper format

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	checkstr := "Welcome to Group A's Week 4 Project!"
 	fmt.Println("Welcome to Group A's Week 4 Project!")
-	fmt.Println(Reversedstr(checkstr))
-	fmt.Println(Palindrome(checkstr))
+	fmt.Println("Reversed String: ", Reversedstr(checkstr))
+	fmt.Println("Palindrome: ", Palindrome(checkstr))
+
+	fmt.Println("Replacing the Word from String:", ReplaceWord(checkstr, "t", "AA"))
 }
