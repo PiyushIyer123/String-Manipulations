fc42f83e1fd6c021d09fc351426e1cfbaa451466
Shiju Daniel Kunnathupadickal <ponnisajeevan12@gmail.com>
2024-09-26 15:45:34 -0400

    Concat Strings!

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to Group A's Week 4 Project!")
+
+	// CONCATENATE STRINGS - By Ponni Sajeevan
+	str1 := "The course is WINP2000!\n"                                  //String 1
+	str2 := "The contributors are: "                                     //String 2
+	str3 := "Ponni, Rose, Nikhitha, Dipanshu, Piyush, Swaroop, Abhishek" //String 3
+	concatoutput := str1 + str2 + str3                                   //Concat Strings and save it to the variable concatoutput
+	fmt.Println(concatoutput)                                            //Print the output
 }
