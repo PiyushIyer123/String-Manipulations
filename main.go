4ab9a8e022fd771fb833ecc498584b060ace7d13
Swaroopkrishna Menon <menonswaroop9517@gmail.com>
2024-09-26 15:44:57 -0400

    ReplaceWord Function Added

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Welcome to Group A's Week 4 Project!")
-	fmt.Println(Reversedstr("Welcome to Group A's Week 4 Project!"))
+	//fmt.Println(Reversedstr("Welcome to Group A's Week 4 Project!"))
+	fmt.Println(ReplaceWord("Group A", "A", "C"))
 }
